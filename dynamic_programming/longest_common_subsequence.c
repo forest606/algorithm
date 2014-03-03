01dbf7cab6446d738efb599c52193ab61292dc66
Liu Forest <forest.liu@sonymobile.com>
2014-03-03 10:09:15 +0800

    Fix longest common subsequence issue

--- dynamic_programming/longest_common_subsequence.c
+++ dynamic_programming/longest_common_subsequence.c
@@ -3,8 +3,8 @@
 
 /*
 A[m], B[n]
-count[0][i] = 0|1, i = 0, 1, 2, ..., m
-count[i][0] = 0|1, i = 0, 1, 2, ..., n
+count[0][i] = 0, i = 0, 1, 2, ..., m
+count[i][0] = 0, i = 0, 1, 2, ..., n
 count[i][j] = count[i - 1][j - 1] + 1, 0 < i < m, 0 < j < n, A[i] == B[j]
 count[i][j] = max(count[i - 1][j], count[i][j - 1]), 0 < i < m, 0 < j < n, A[i] != B[j]
 */
@@ -12,32 +12,15 @@ count[i][j] = max(count[i - 1][j], count[i][j - 1]), 0 < i < m, 0 < j < n, A[i] 
 extern void **malloc_2d_array(size_t row, size_t column, size_t size);
 extern void free_2d_array(void **p, size_t row);
 
-#define DEFAULT_VALUE -1
+#define DEFAULT_VALUE 0
 enum {MOVE_LEFT, MOVE_UP, MOVE_DIAGONAL};
-char *move = "-I\\";
+char *move = "-|\\";
 
 int lcs_length_recursive(char *astr, char *bstr, int **count, int aindex, int bindex, char **output) {
   int i, amax, bmax;
+  if(aindex < 0 || bindex < 0)
+    return 0;
   if(count[aindex][bindex] == DEFAULT_VALUE) {
-    if(aindex == 0) {
-      for(i = bindex; i >= 0; --i) {
-        if(bstr[i] == astr[aindex]) {
-          output[aindex][bindex] = move[MOVE_DIAGONAL];
-          return 1;
-        }
-      }
-      return 0;
-    }
-    if(bindex == 0) {
-      for(i = aindex; i >= 0; --i) {
-        if(astr[i] == bstr[bindex]) {
-          output[aindex][bindex] = move[MOVE_DIAGONAL];
-          return 1;
-        }
-      }
-      return 0;
-    }
-
     if(astr[aindex] == bstr[bindex]) {
       count[aindex][bindex] = lcs_length_recursive(astr, bstr, count, aindex - 1, bindex - 1, output) + 1;
       output[aindex][bindex] = move[MOVE_DIAGONAL];
@@ -57,15 +40,16 @@ int lcs_length_recursive(char *astr, char *bstr, int **count, int aindex, int bi
 }
 
 void print_lcs(char *astr, char **output, int m, int n) {
-  if(m == 0 || n == 0)
+  if(m < 0 || n < 0) {
     return;
-  if(output[m][n] == MOVE_UP) {
-    print_lcs(astr, output, m - 1, n);
-  } else if(output[m][n] == MOVE_LEFT) {
-    print_lcs(astr, output, m, n - 1);
-  } else {
+  }
+  if(output[m][n] == move[MOVE_DIAGONAL]) {
     print_lcs(astr, output, m - 1, n - 1);
     printf("%c", astr[m]);
+  } if(output[m][n] == move[MOVE_UP]) {
+    print_lcs(astr, output, m - 1, n);
+  } else {
+    print_lcs(astr, output, m, n - 1);
   }
 }
 
@@ -107,9 +91,7 @@ int lcs_length(char *astr, char *bstr) {
   return len;
 }
 
-int main() {
-  char *astr = "ABCBDAB";
-  char *bstr = "BDCABA";
-  printf("Max longest common subsequence is %d\n", lcs_length(astr, bstr));
+int main(int argc, char **argv) {
+  printf("Max longest common subsequence is %d\n", lcs_length(argv[1], argv[2]));
   return 0;
 }
